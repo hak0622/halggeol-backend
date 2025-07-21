7dc8eba5bb81533f39c7182ed81e8ccdf0eff1a1
MINUUUUUUUUUUUU <m2nuuu@gmail.com>
2025-07-21 13:45:37 +0900

    chore: 누락된 alias 어노테이션 작성

--- src/main/java/com/halggeol/backend/domain/RecommendProduct.java
+++ src/main/java/com/halggeol/backend/domain/RecommendProduct.java
@@ -3,10 +3,12 @@ package com.halggeol.backend.domain;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.ibatis.type.Alias;
 
 @Getter
 @Setter
 @Builder
+@Alias("RecommendProductVO")
 public class RecommendProduct {
     private String productId;
     private String status;  // ENUM('가입', '관심', '회고')
