1e0fd83bad0823cc8d2b498d5e3c783c4ccea408
MINUUUUUUUUUUUU <m2nuuu@gmail.com>
2025-07-22 11:45:21 +0900

    feat: [FFBV-27] 적금 상세보기 API 구현

--- src/main/java/com/halggeol/backend/products/service/ProductDetailServiceImpl.java
+++ src/main/java/com/halggeol/backend/products/service/ProductDetailServiceImpl.java
@@ -1,7 +1,7 @@
 package com.halggeol.backend.products.service;
 
-import com.halggeol.backend.domain.Deposit;
 import com.halggeol.backend.products.dto.DepositDetailResponseDTO;
+import com.halggeol.backend.products.dto.SavingsDetailResponseDTO;
 import com.halggeol.backend.products.mapper.ProductDetailMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,6 +19,11 @@ public class ProductDetailServiceImpl implements ProductDetailService {
 
         return productDetailMapper.selectDepositDetailById(depositId, userId);
     }
-
+    @Override
+    @Transactional(readOnly = true)
+    public SavingsDetailResponseDTO getSavingsDetailById(String savingsId, String userId) {
+        // 매퍼에서 이미 조인 및 DTO 매핑을 완료하므로, 별도의 변환 없이 반환
+        return productDetailMapper.selectSavingsDetailById(savingsId, userId);
+    }
 
 }
