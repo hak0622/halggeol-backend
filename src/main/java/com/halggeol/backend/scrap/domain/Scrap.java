072e1cc8230bef063d786e800557d09e2b93e4be
MINUUUUUUUUUUUU <m2nuuu@gmail.com>
2025-07-23 10:13:40 +0900

    test: [FFBV-101] ScrapServiceImpl, ScrapController 테스트 코드 작성

--- src/main/java/com/halggeol/backend/scrap/domain/Scrap.java
+++ src/main/java/com/halggeol/backend/scrap/domain/Scrap.java
@@ -2,6 +2,7 @@ package com.halggeol.backend.scrap.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,6 +14,7 @@ import org.apache.ibatis.type.Alias;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@EqualsAndHashCode // 객체 동등성 비교를 위해서
 public class Scrap {
     private int userId;
     private String productId;
